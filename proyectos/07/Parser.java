547b1e5b7a04d56d49feb67d49ef9ef20ac79930
jalcara1 <jalcara1@f98ad051-82a6-4b86-8fc6-6719155bd127>
2017-09-28 14:00:56 +0000

    File reading

--- proyectos/07/Parser.java
+++ proyectos/07/Parser.java
@@ -1,10 +1,37 @@
+import java.util.IllegalFormatException;
 import java.io.FileNotFoundException;
-import java.util.ArrayList;
 import java.util.Scanner;
 import java.io.File;
 
 public class Parser{
+    private Scanner commands;
+    
     public Parser(File inputFile){
+	try{
+	    commands = new Scanner(inputFile);
+	    String line ="", codes ="";	    
+	    while(commands.hasNext()){
+		line = comment(cmds.nextLine()).trim();
+		if(line.length() > 0){
+		    codes +=line +"\n";
+		}
+	    }
+	    commands = new Scanner(codes.trim());
+	}catch(FileNotFoundException e){
+	    System.out.println("File not found");
+	}
+    }
+    public String comment(String line){
+	int pos = line.indexOf("//");
+	if (pos != -1){
+	    line = line.substring(0, pos);
+	}
+	return line;
+    }
+    public boolean moreCommands(){
+	return commands.hasNextLine();
+    }
+    public void advance(){
 
     }
 }
--- proyectos/07/VMtranslator.java
+++ proyectos/07/VMtranslator.java
@@ -1,9 +1,18 @@
-import java.util.ArrayList;
-import java.util.HashMap;
 import java.io.File;
 
 public class VMtranslator{
     public static void main(String args[]){
-
+	if(args.length != 1){
+	    Systme.out.println("Not input file\n Usage: java VMTranslator *.vm");
+	}else{
+	    File file =new File(args[0]);
+	    String pathFileOut =file.getAbsolutePath().substring(0, file.getAbsolutePath().lastIndexOf(".")) + ".asm";
+	    File fileOut =new File(fileOutPath);
+	    Parser parser =new Parser(file);
+	    while(parser.moreCommands){
+		
+	    }	    
+	    System.out.println("File Created: " + fileOutPath);
+	}
     }
 }
