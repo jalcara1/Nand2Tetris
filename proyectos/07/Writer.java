534134f21ab863739d7df5f8d590040c32cfd1c3
jalcara1 <jalcara1@f98ad051-82a6-4b86-8fc6-6719155bd127>
2017-09-28 19:35:49 +0000

    Writer Started

--- proyectos/07/Writer.java
+++ proyectos/07/Writer.java
@@ -3,7 +3,14 @@ import java.io.PrintWriter;
 import java.io.File;
 
 public class Writer{
+    private PrintWriter output;
+    private in jumpFlags;
     public Writer(File outputFile){
-
+	try{
+	    output =new PrintWriter(outputFile);
+	    jumpFlags =0;
+	}catch(FileNotFoundException e){
+	    e.printStackTrace();
+	}
     }
 }
